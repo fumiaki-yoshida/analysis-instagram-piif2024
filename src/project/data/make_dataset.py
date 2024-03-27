5b43402330e8dc7b81045fb646932419f9001c85
yoshida@newrope <yoshida@newrope.biz>
2024-03-27 19:38:43 +0900

    fix test

--- src/project/data/make_dataset.py
+++ src/project/data/make_dataset.py
@@ -1,26 +1,26 @@
 import re
 import pandas as pd
 
-def _split_label_names(label_names:str):
+
+def _split_label_names(label_names: str):
     return label_names.split("/")
 
 
-def _label_is_in_columns(columns:list, label:str):
+def _label_is_in_columns(columns: list, label: str):
     return label in columns
 
 
-def _add_new_labels(columns:list, label:str):
-    if _label_is_in_columns(columns,label):
+def _add_new_labels(columns: list, label: str):
+    if _label_is_in_columns(columns, label):
         return columns
     else:
-        return columns+[label]
-    
+        return columns + [label]
+
 
 def _remove_category_number(label):
-    return re.sub("([a-z]+)_[0-9]" ,r"\1" ,label)
+    return re.sub("([a-z]+)_[0-9]", r"\1", label)
 
 
 class RecognizedData:
-    def __init__(self,path):
+    def __init__(self, path):
         self.df = pd.read_csv(path)
-
--- tests/data/test_make_dataset.py
+++ tests/data/test_make_dataset.py
@@ -2,29 +2,44 @@ import pandas as pd
 from src.project.data import make_dataset
 
 
-column_list =['createdat', 'source', 'category',
-              'label_names', 'tags', 'image_url',
-              'weburl', 'name',"id","gender"]
+column_list = [
+    "createdat",
+    "source",
+    "category",
+    "label_names",
+    "tags",
+    "image_url",
+    "weburl",
+    "name",
+    "id",
+    "gender",
+]
+
 
 def test_split_labels():
-    assert make_dataset._split_label_names("エコバッグ/キャンバス") == ["エコバッグ","キャンバス"]
+    assert make_dataset._split_label_names("エコバッグ/キャンバス") == ["エコバッグ", "キャンバス"]
     assert make_dataset._split_label_names("エコバッグ") == ["エコバッグ"]
 
 
 def test_label_is_in_columns():
-    dummy_columns = ["bag_エコバッグ","bag_キャンパス"]
-    assert make_dataset._label_is_in_columns(dummy_columns,"bag_エコバッグ")
-    assert not make_dataset._label_is_in_columns(dummy_columns,"bag_クリア")
+    dummy_columns = ["bag_エコバッグ", "bag_キャンパス"]
+    assert make_dataset._label_is_in_columns(dummy_columns, "bag_エコバッグ")
+    assert not make_dataset._label_is_in_columns(dummy_columns, "bag_クリア")
 
 
 def test_add_new_labels():
-    dummy_columns = ["bag_エコバッグ","bag_キャンパス"]
-    assert make_dataset._add_new_labels(dummy_columns,"bag_エコバッグ") == dummy_columns
-    assert make_dataset._add_new_labels(dummy_columns,"bag_キャンパス") == dummy_columns
-    assert make_dataset._add_new_labels(dummy_columns,"bag_クリア") == ["bag_エコバッグ","bag_キャンパス","bag_クリア"]
+    dummy_columns = ["bag_エコバッグ", "bag_キャンパス"]
+    assert make_dataset._add_new_labels(dummy_columns, "bag_エコバッグ") == dummy_columns
+    assert make_dataset._add_new_labels(dummy_columns, "bag_キャンパス") == dummy_columns
+    assert make_dataset._add_new_labels(dummy_columns, "bag_クリア") == [
+        "bag_エコバッグ",
+        "bag_キャンパス",
+        "bag_クリア",
+    ]
 
 
 def test_remove_category_number():
-    assert make_dataset._remove_category_number("top_1")=="top"
-    assert make_dataset._remove_category_number("top_2")=="top"
-    assert make_dataset._remove_category_number("top_3")=="top"
+    assert make_dataset._remove_category_number("top_1") == "top"
+    assert make_dataset._remove_category_number("top_2") == "top"
+    assert make_dataset._remove_category_number("bottoms_3") == "bottoms"
+    assert make_dataset._remove_category_number("bottoms_10") != "bottoms"
